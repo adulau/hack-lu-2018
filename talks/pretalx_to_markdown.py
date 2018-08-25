9befa90b40672946632225dffb56b3b8233760f0
Raphaël Vinot <raphael@vinot.info>
2018-08-24 21:58:00 -0400

    new: quick and dirty MD generator

--- talks/pretalx_to_markdown.py
+++ talks/pretalx_to_markdown.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8
 
 import json
+from urllib.parse import quote_plus
 
 # Get talks from here: https://cfp.hack.lu/api/events/hacklu18/talks/?limit=100
 # TODO: automate that.
@@ -20,7 +21,7 @@ all_workshops = '''
 '''
 
 talk_template = '''
-## {title}
+## <a name="{escaped_title}"></a>{title}
 by {speaker}
 
 {abstract}
@@ -28,7 +29,7 @@ by {speaker}
 '''
 
 speaker_template = '''
-### Bio: {speaker}
+### Bio: <a name="{escaped_speaker}"></a>{speaker}
 
 {bio}
 
@@ -38,9 +39,14 @@ for talk in talks['results']:
     if talk['state'] != 'confirmed':
         print('NOT CONFIRMED', talk['title'])
         continue
-    md_talk = talk_template.format(title=talk['title'], speaker=', '.join([speaker['name'] for speaker in talk['speakers']]), abstract=talk['abstract'])
+    md_talk = talk_template.format(escaped_title=quote_plus(talk['title']),
+                                   title=talk['title'],
+                                   speaker=', '.join([speaker['name'] for speaker in talk['speakers']]),
+                                   abstract=talk['abstract'])
     for speaker in talk['speakers']:
-        md_talk += speaker_template.format(speaker=speaker['name'], bio=speaker['biography'])
+        md_talk += speaker_template.format(speaker=speaker['name'],
+                                           escaped_speaker=quote_plus(speaker['name']),
+                                           bio=speaker['biography'])
 
     if talk['submission_type']['en'] in ['Short talk', 'Talk']:
         all_talks += md_talk
